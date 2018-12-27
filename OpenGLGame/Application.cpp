156dc06da78bea3ae69ce5838d30bcbf3227bee0
haowenlai2008 <719590102@qq.com>
2018-12-27 14:44:29 +0800

    Demo
    创建项目

---
+++ OpenGLGame/Application.cpp
@@ -0,0 +1,11 @@
+
+#include "glfw3.h"
+#include "glfw3native.h"
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_TRUE);
+	return 0;
+}
