a93e9955a8819ba8097e24cbcd6f4570481746cf
haowenlai2008 <719590102@qq.com>
2018-12-27 19:42:51 +0800

    搭环境完成

--- Project/Application.cpp
+++ Project/Application.cpp
@@ -1,10 +1,38 @@
+#include<iostream>
+#define GLEW_STATIC
+//#include <glew.h>
+//#include <glfw3.h>
 #include "glfw3.h"
+#include "glad.h"
+using namespace std;
 
-int main()
+int main(int argc, char** argv[])
 {
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_TRUE);
+	glfwInit();//≥ı ºªØ
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);//≈‰÷√GLFW
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);//≈‰÷√GLFW
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);//
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		cout << "Failed to create GLFW window" << endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+	glfwTerminate();
 	return 0;
 }
