95bec2b866e98f043e524af4a19fa8c3f2b9ac25
haowenlai2008 <719590102@qq.com>
2019-03-24 12:18:46 +0800

    更新一波

--- Triangle/FrameWork/Application.cpp
+++ Triangle/FrameWork/Application.cpp
@@ -31,13 +31,13 @@ void BaseInit()
 		return;
 	}
 }
-
-void MainLoop()
+//普通的三角形
+void NormalTriangle()
 {
 	float Triangle[] = {
-	-0.9f, -0.5f, 0.0f,  // left 
-	-0.0f, -0.5f, 0.0f,  // right
-	-0.45f, 0.5f, 0.0f,  // top 
+-0.9f, -0.5f, 0.0f,  // left 
+-0.0f, -0.5f, 0.0f,  // right
+-0.45f, 0.5f, 0.0f,  // top 
 	};
 
 	//编译着色器
@@ -67,6 +67,159 @@ void MainLoop()
 		glfwSwapBuffers(glWindow);
 	}
 }
+//五颜六色的三角形
+void ColourfulTriangle()
+{
+	//颜色会在光栅化阶段被硬件进行插值计算
+	float vertices[] = {
+		// 位置              // 颜色
+		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // 右下
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // 左下
+		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // 顶部
+	};
+
+	//编译着色器
+	Shader ourShader("vertex_4.vs", "fragment_4.fs");
+	ourShader.use();//glUseProgram(shaderProgram);
+	unsigned int VBO, VAO;
+	//顶点数组
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	//绑定顶点数组缓存
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// 位置属性
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);//设置数据访问的指针
+	glEnableVertexAttribArray(0);
+	// 颜色属性
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));//设置数据访问的指针
+	glEnableVertexAttribArray(1);
+
+
+	while (!glfwWindowShouldClose(glWindow))
+	{
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		//draw
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glBindVertexArray(0);
+		glfwPollEvents();
+		glfwSwapBuffers(glWindow);
+	}
+}
+//利用两个顶点数组对象VAO画两个三角形
+void TwoTriangle()
+{
+	float firstTriangle[] = {
+	-0.9f, -0.5f, 0.0f,  // left 
+	-0.0f, -0.5f, 0.0f,  // right
+	-0.45f, 0.5f, 0.0f,  // top 
+	};
+	float secondTriangle[] = {
+		0.0f, -0.5f, 0.0f,  // left
+		0.9f, -0.5f, 0.0f,  // right
+		0.45f, 0.5f, 0.0f   // top 
+	};
+
+	//编译着色器
+	Shader ourShader("vertex_1.vs", "fragment_1.fs");
+	ourShader.use();//glUseProgram(shaderProgram);
+	unsigned int VBOs[2], VAOs[2];
+	glGenVertexArrays(2, VAOs);
+	glGenBuffers(2, VBOs);
+	//第一个顶点数组对象属性的设置
+	glBindVertexArray(VAOs[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	//第二个顶点数组对象属性的设置
+	glBindVertexArray(VAOs[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(secondTriangle), secondTriangle, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glEnableVertexAttribArray(0);
+
+
+
+	while (!glfwWindowShouldClose(glWindow))
+	{
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		//draw
+		glBindVertexArray(VAOs[0]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// then we draw the second triangle using the data from the second VAO
+		glBindVertexArray(VAOs[1]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwPollEvents();
+		glfwSwapBuffers(glWindow);
+	}
+}
+//使用索引缓存画两个三角形
+void TwoTriangleByE()
+{
+	float vList[] = {
+	0.5f,0.5f,0.0f,
+	0.5f,-0.5f,0.0f,
+	-0.5f,-0.5f,0.0f,
+	-0.5f,0.5f,0.0f,
+	0.0f,0.7f,0.0f
+	};
+
+	unsigned int indices[] = {
+		0,1,4,
+		1,2,3
+	};
+	//编译着色器
+	Shader ourShader("vertex_1.vs", "fragment_1.fs");
+	ourShader.use();//glUseProgram(shaderProgram);
+	//顶点数组
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	//顶点缓存
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vList), vList, GL_STATIC_DRAW);
+
+	//索引缓存
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	//设置顶点属性指针
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);//画线
+	while (!glfwWindowShouldClose(glWindow))
+	{
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		//draw
+		glBindVertexArray(VAO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+
+		glfwPollEvents();
+		glfwSwapBuffers(glWindow);
+	}
+}
+void MainLoop()
+{
+	ColourfulTriangle();
+}
+
 int main(int argc, char** argv[])
 {
 	BaseInit();
