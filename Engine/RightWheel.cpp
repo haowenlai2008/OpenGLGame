c917d6150759d4da1c12a03eabba4f487b9ba188
laihaowen <laihaowen@netease.corp.com>
2021-09-24 14:45:31 +0800

    修bug

--- Engine/RightWheel.cpp
+++ Engine/RightWheel.cpp
@@ -4,22 +4,21 @@
 bool RightWheel::init()
 {
 	CylinderNoCap* cnc = CylinderNoCap::create(Entity_Type::WithTexAndLight);
+	this->addChild(cnc);
 	cnc->setTexture("wheel.jpg");
 	cnc->setLocalPosition(vec3(0.25f, 0.0f, 0.0f));
 	cnc->setScale(vec3(0.5f, 1.0f, 1.0f));
-	this->addChild(cnc);
 
 	Circle* c1 = Circle::create(Entity_Type::WithTexAndLight);
+	this->addChild(c1);
 	c1->setTexture("wheelface.jpg");
 	c1->setLocalPosition(vec3(0.5f, 0.0f, 0.0f));
-	this->addChild(c1);
 
 	Circle* c2 = Circle::create(Entity_Type::WithTexAndLight);
+	this->addChild(c2);
 	c2->setTexture("wheelface.jpg");
 	c2->setLocalPosition(vec3(0.0f, 0.0f, 0.0f));
 	c2->setRotate(vec3(0.0f, 180.0f, 0.0f));//翻转180度
-	//c2->setDebugID(100);
-	this->addChild(c2);
 
 	setRadius(0.5f);
 	return true;
