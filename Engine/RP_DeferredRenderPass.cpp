fed25f4a0e85988532b0f0ea079b374d38c02079
haowenlai2008 <719590102@qq.com>
2023-12-20 02:27:22 +0800

    延迟渲染后用前向渲染画天空盒，复制GBuffer的深度缓冲

--- Engine/RP_DeferredRenderPass.cpp
+++ Engine/RP_DeferredRenderPass.cpp
@@ -59,12 +59,30 @@ bool RP_DeferredRenderPass::Init()
 bool RP_DeferredRenderPass::Render()
 {
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+	// 把GBuffer的深度缓冲复制到SceneBuffer
+	BaseManager* bmp = BaseManager::getInstance();
+	float width = bmp->screenWidth;
+	float height = bmp->screenHeight;
+	glBindFramebuffer(GL_READ_FRAMEBUFFER, RenderManager::globalBuffer.gBuffer);
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, RenderManager::globalBuffer.scenePassBuffer);
+	glBlitFramebuffer(0, 0, width, height, 0, 0, width, height, GL_DEPTH_BUFFER_BIT, GL_NEAREST);
+	glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+
+
 	// now bind back to default framebuffer and draw a quad plane with the attached framebuffer color texture
 	glBindFramebuffer(GL_FRAMEBUFFER, RenderManager::globalBuffer.scenePassBuffer);
-	glDisable(GL_DEPTH_TEST); // disable depth test so screen-space quad isn't discarded due to depth test.
-	// clear all relevant buffers
+
+	// 关闭深度测试和深度写入，不给光照部分覆盖GBuffer的深度缓冲
+	glDisable(GL_DEPTH_TEST);
+	glDepthMask(GL_FALSE);	
+
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+
 
 	BaseManager* baseManager = BaseManager::getInstance();
 	MaterialManager* matManager = MaterialManager::getInstance();
@@ -86,6 +104,29 @@ bool RP_DeferredRenderPass::Render()
 
 	glBindVertexArray(quadVAO);
 	glDrawArrays(GL_TRIANGLES, 0, 6);
+
+	// 打开深度写入和深度测试，画天空盒
+	glEnable(GL_DEPTH_TEST);
+	glDepthMask(GL_TRUE);	
+	// 最后前向渲染天空盒
+	auto p = RenderManager::getInstance()->skyBox;
+	if (p != nullptr && p->count != 0)
+	{
+		auto ent_ptr = dynamic_cast<Entity*>(p);
+		auto& material = *ent_ptr->GetMaterial().lock();
+		auto shader = material.m_Shader.lock();
+		glm::mat4 model = p->getModelMatrix();
+		shader->use();
+		material.bindUniform();
+		shader->setMat4("projection", projection);
+		shader->setMat4("view", view);
+		shader->setMat4("model", model);
+		shader->setMat4("lightSpaceMatrix", lightSpace);
+		shader->setVec3("viewPos", viewPos);
+		shader->setVec3("light.position", lightPos);
+		p->draw();
+	}
+
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
     return true;
 }
